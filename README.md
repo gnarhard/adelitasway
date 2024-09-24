83ffbd9a4b78770877a5a9a9925db5d727e0dbbe
Grayson Erhard <develop@graysonerhard.com>
2024-09-23 23:43:19 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Adelitas Way Official Website
+
+### todo
+- mailchimp signup form
---
+++ resources/views/layout/footer.php
@@ -0,0 +1,11 @@
+<!-- Footer -->
+<footer class="bg-dark py-4">
+    <div class="container text-center">
+        <p class="mb-0 text-light">&copy; <?php echo date('Y'); ?> Adelitas Way. All rights reserved.</p>
+    </div>
+</footer>
+
+<!-- Latest Bootstrap 5 JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<!-- Font Awesome -->
+<script src="https://kit.fontawesome.com/fbd2e4f78f.js" crossorigin="anonymous"></script>
